cb2e64dd6a4657a82aafa9ab37adb89f65dd466a
PAULINA SALAZAR <83146423+MyPaulina@users.noreply.github.com>
2021-10-27 20:36:34 -0500

    Add files via upload

---
+++ 02.template-string.js
@@ -0,0 +1,18 @@
+
+const nombre = 'Pau';
+const apellido = 'Slz';
+
+const nombreCompleto = nombre + '' + apellido;
+
+
+console.log('nombreCompleto');
+
+
+ function getSaludo () {
+    return 'Hola mundo';
+ }
+
+ console.log ( 'Este es un texto: ${ getSaludo()}' ) ;
+
+
+ 
